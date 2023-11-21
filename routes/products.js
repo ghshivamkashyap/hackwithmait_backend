75f97d173d391ac6395ddb60591ce44578e37517
SHIVAM KASHYAP <107611606+ghshivamkashyap@users.noreply.github.com>
2023-11-21 18:44:31 +0530

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { getProductById, addProduct, genrateBiill } = require("../controllers/products");
+const { getProductById, addProduct, genrateBiill, securityCheck } = require("../controllers/products");
 
 // Use a route parameter for 'pid'
 router.get("/getproductbyid/:pid", getProductById);
+router.get("/security/:billId", securityCheck);
 router.post("/addproduct", addProduct);
 router.post("/getbill", genrateBiill);
 
